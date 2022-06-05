d06869d8a200a263365caedd4a8862587c6cdfc2
Jiecao-He <419648726@qq.com>
2022-06-05 13:28:56 +0800

    Update url.js

--- url.js
+++ url.js
@@ -7,7 +7,23 @@
  */
 
 
+/*
+*網站順序
+*咖啡風衣
+*風衣咖啡
+*牙膏印表機
+*印表機牙膏
+*啤酒起泡水
+*起泡水啤酒
+*
+*/	
+
+
 const urls = [
-	'https://www.baidu.com',
-	'https://www.google.com'
+	'https://docs.google.com/forms/d/e/1FAIpQLScbEF7TlNtJXfcmDykKChbIV7fBmSHf9S-WsbatsJs_aq4TEA/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeiHNin6-z97XyHWFIIAWx1Pq2UTaev7g4F8gRblik9d1jOIA/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLScW6fgzVcljhEeuuAZPFrlMzVy0TN-X4yAmMgCDCjD8KGQU4A/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLScY4w-ZmeYmYL1PEvZ4A8jmkdaZYooHeWV5zsUoiahYWnUDuA/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSd-vOpJcdXbxesrZhZDwfWpEo6S4WDZ5KkcRP4GCZsePw45Gw/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLScyEgYY1A8HSGcdLVCa7OyY5GQzhtkFPif_dbh3kEAQWPGgdA/viewform'
 ];
