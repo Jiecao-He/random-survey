368904dd8dba9afae0e62b41e3bf2f5d184e5669
He <419648726@qq.com>
2023-04-24 20:03:57 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,21 +9,20 @@
 
 /*
 *網站順序
-*咖啡風衣
-*風衣咖啡
-*牙膏印表機
-*印表機牙膏
-*啤酒起泡水
-*起泡水啤酒
-*
-*/	
+*低描述近延伸：https://docs.google.com/forms/d/e/1FAIpQLSeN_AvFTUXJ_lqf6fXCvfmpBf4SsqGcdjFvGImVY6seH3uFaw/viewform
+*低描述中延伸：https://docs.google.com/forms/d/e/1FAIpQLSdwevKIx0X8KuOnp81_maB7Raba_j2Wc8vlAxg-oRYoN4IRKw/viewform
+*低描述遠延伸：https://docs.google.com/forms/d/e/1FAIpQLSeyRYbtK2MhfoWm3kTkU6oY5j93HfLZHVTQDo88F4nLCQ0d9Q/viewform
+*高描述近延伸：https://docs.google.com/forms/d/e/1FAIpQLSejb9pV6NsysLHy3XCvf8jLIyTXkXmxDSCG2urpmB4irGbLNw/viewform
+*高描述中延伸：https://docs.google.com/forms/d/e/1FAIpQLSfiHUckPCl7uOtRFA6AXzQcYhvGY3zspHQCvhzDf22kChlHcg/viewform
+*高描述遠延伸：https://docs.google.com/forms/d/e/1FAIpQLSeVj0g24BpOKrx0ts6_Yz6pgSv_5l-kvCGWvbBPFKLtaQqy6w/viewform
+*/		
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLScbEF7TlNtJXfcmDykKChbIV7fBmSHf9S-WsbatsJs_aq4TEA/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSeiHNin6-z97XyHWFIIAWx1Pq2UTaev7g4F8gRblik9d1jOIA/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLScW6fgzVcljhEeuuAZPFrlMzVy0TN-X4yAmMgCDCjD8KGQU4A/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLScY4w-ZmeYmYL1PEvZ4A8jmkdaZYooHeWV5zsUoiahYWnUDuA/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSd-vOpJcdXbxesrZhZDwfWpEo6S4WDZ5KkcRP4GCZsePw45Gw/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLScyEgYY1A8HSGcdLVCa7OyY5GQzhtkFPif_dbh3kEAQWPGgdA/viewform'
+	'https://docs.google.com/forms/d/e/1FAIpQLSeN_AvFTUXJ_lqf6fXCvfmpBf4SsqGcdjFvGImVY6seH3uFaw/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdwevKIx0X8KuOnp81_maB7Raba_j2Wc8vlAxg-oRYoN4IRKw/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeyRYbtK2MhfoWm3kTkU6oY5j93HfLZHVTQDo88F4nLCQ0d9Q/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSejb9pV6NsysLHy3XCvf8jLIyTXkXmxDSCG2urpmB4irGbLNw/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSfiHUckPCl7uOtRFA6AXzQcYhvGY3zspHQCvhzDf22kChlHcg/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeVj0g24BpOKrx0ts6_Yz6pgSv_5l-kvCGWvbBPFKLtaQqy6w/viewform'
 ];
